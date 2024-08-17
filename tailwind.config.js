03c7186ee11cc9d8c20d8594cea6bbc6cc7d25a8
Jakub Olejnik <87308273+JakubOlejnik2007@users.noreply.github.com>
2024-08-17 23:47:43 +0200

    Added base for orange section

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,11 @@ module.exports = {
       "text_on_orange": "rgba(246,255,255,0.77)",
       "text_on_gray": "#afb5b5",
       "my_white": "#c9ccc8",
-      "my_orange": "ff983e",
+      "my_orange": "#ff983e",
       "my_black": "#252525",
-      "my_brown": "2b190a",
+      "my_brown": "#2b190a",
+      "border": "#3b3b3a",
+      "header": "#fdfdfd",
     }
   },
   plugins: [],
