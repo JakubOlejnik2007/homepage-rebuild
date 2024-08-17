8a688a9064013ddc041c48b305928095127b2f91
Jakub Olejnik <87308273+JakubOlejnik2007@users.noreply.github.com>
2024-08-17 22:57:22 +0200

    Added navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
   theme: {
     extend: {},
     colors: {
+      "nav_font": "#919091",
+      "text_on_orange": "rgba(246,255,255,0.77)",
+      "text_on_gray": "#afb5b5",
       "my_white": "#c9ccc8",
       "my_orange": "ff983e",
       "my_black": "#252525",
