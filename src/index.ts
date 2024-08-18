50db181f2a84ca1399c4d497c09f1349f33f594b
Jakub Olejnik <87308273+JakubOlejnik2007@users.noreply.github.com>
2024-08-18 20:23:28 +0200

    Added new sections and test code for sqlite db

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,25 @@ app.get("/get-posts", (req, res) => {
     ])
 })
 
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('homepagedata.db');
+
+db.serialize(() => {
+    db.run("CREATE TABLE lorem (info TEXT)");
+
+    const stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+    for (let i = 0; i < 10; i++) {
+        stmt.run("Ipsum " + i);
+    }
+    stmt.finalize();
+
+    db.each("SELECT rowid AS id, info FROM lorem", (_err: any, row: { id: string; info: string; }) => {
+        console.log(row.id + ": " + row.info);
+    });
+});
+
+db.close();
+
 app.listen(config.port, () =>
     console.log(`[⚡] Server is listening on port: ${config.port}!`)
 )
